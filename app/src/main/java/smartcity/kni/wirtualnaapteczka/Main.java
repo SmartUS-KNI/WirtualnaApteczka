6e8e3a5bea7d2a4d6bd2648eaf5473e117133040
irakli1990 <iraklikardawa@gmail.com>
2018-01-23 13:18:20 +0100

    GUI Fixed version <iraklikardawa@gmail.com>
    Added:
    New in velue folder file ids.xml. Person who will be programing activities can find there ids of components. ids are comented above, so you can check which id from wich activity.Id's of components are written in camel case with underscores. exaple //add_Medicine_Main_button
    Deleted:
    In folder values, from file string.xml
    redundant string ids were removed.
    Modified:
    Any modification were not done,just fixed layout, removed not corect definitions. layout is tested on LG_lfino.

--- app/src/main/java/smartcity/kni/wirtualnaapteczka/Main.java
+++ app/src/main/java/smartcity/kni/wirtualnaapteczka/Main.java
@@ -1,7 +1,10 @@
 package smartcity.kni.wirtualnaapteczka;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_medicine_info);
+//        setContentView(R.layout.activity_medicine_list);
+//        setContentView(R.layout.activity_new_medicine_form);
     }
+
+
 }
