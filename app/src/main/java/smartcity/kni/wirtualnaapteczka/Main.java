b863fe46b9e447cdba81602ed097a2fbf67d0878
MateuszLebioda <mateusz.lebioda.ml@gmail.com>
2018-01-31 13:21:35 +0100

    Create AddMedicineActivity
    Add:
    -> Activity AddMedicineActivity
    Edit:
    -> Activity Main
             -> I added public void OpenAddMedicineActivity() metodh,
             to open new activity.
    -> acitvity_main.xml
              ->I added one option onClick to open new activity .
    -> string.xml
              -> I added title AddMedicineActivity.

--- app/src/main/java/smartcity/kni/wirtualnaapteczka/Main.java
+++ app/src/main/java/smartcity/kni/wirtualnaapteczka/Main.java
@@ -1,7 +1,11 @@
 package smartcity.kni.wirtualnaapteczka;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import smartcity.kni.wirtualnaapteczka.listeners.AddMedicineActivity;
 
 public class Main extends AppCompatActivity {
 
@@ -12,6 +16,11 @@ public class Main extends AppCompatActivity {
         //setContentView(R.layout.activity_medicine_info);
         //setContentView(R.layout.activity_medicine_list);
         //setContentView(R.layout.activity_new_medicine_form);
+    }
+
+    public void OpenAddMedicineActivity(View view){
+        Intent intent = new Intent(this,AddMedicineActivity.class);
+        startActivity(intent);
     }
 
 
---
+++ app/src/main/java/smartcity/kni/wirtualnaapteczka/listeners/AddMedicineActivity.java
@@ -0,0 +1,17 @@
+package smartcity.kni.wirtualnaapteczka.listeners;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import smartcity.kni.wirtualnaapteczka.R;
+
+public class AddMedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_medicine_form);
+    }
+
+
+}
