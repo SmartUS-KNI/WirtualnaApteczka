26210f3cca3a5ac2cba5701091517bf50edadecc
MateuszLebioda <mateusz.lebioda.ml@gmail.com>
2018-02-07 17:39:23 +0100

    I deleted all changes, which I did.
    Deleted:
    -All Activity AddMedicineActivity.java
    -function OpenAddMedicineActivity in Main.java
    -android:onClick="OpenAddMedicineActivity" in activity_main.xml
    - <string name="title_activity_add_medicine">AddMedicineActivity</string> in string.xml
    
    Now project is looking like master.

--- app/src/main/java/smartcity/kni/wirtualnaapteczka/Main.java
+++ app/src/main/java/smartcity/kni/wirtualnaapteczka/Main.java
@@ -1,11 +1,8 @@
 package smartcity.kni.wirtualnaapteczka;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-import smartcity.kni.wirtualnaapteczka.listeners.AddMedicineActivity;
 
 public class Main extends AppCompatActivity {
 
@@ -17,11 +14,5 @@ public class Main extends AppCompatActivity {
         //setContentView(R.layout.activity_medicine_list);
         //setContentView(R.layout.activity_new_medicine_form);
     }
-
-    public void OpenAddMedicineActivity(View view){
-        Intent intent = new Intent(this,AddMedicineActivity.class);
-        startActivity(intent);
-    }
-
 
 }
