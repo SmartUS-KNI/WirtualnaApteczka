1f4b5eb1bd947691c08cf2ca71aabc7462daa733
Tomasz Pawlik <k4masz@gmail.com>
2018-04-23 11:17:23 +0200

    Added:  - Weekly Adjustment Dialog Modified:  - Set project JDK to 1.8  - Refactored Code

--- app/src/main/java/smartcity/kni/wirtualnaapteczka/dialogs/MonthlyDoseAdjustmentDialog.java
+++ app/src/main/java/smartcity/kni/wirtualnaapteczka/dialogs/MonthlyDoseAdjustmentDialog.java
@@ -5,11 +5,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.widget.GridLayout;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CheckBox;
-
 
 import smartcity.kni.wirtualnaapteczka.R;
 
